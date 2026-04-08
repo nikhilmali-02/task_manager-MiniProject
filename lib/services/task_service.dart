1610fecc48032d2f3ba0f63f2dd3a6c2c005cfa7
NikhilMali <nikhilpundalikmali@gmail.com>
2026-04-08 14:36:48 +0530

    7 April 2026 - task_service.dart

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,3 +1,25 @@
-class TaskService {
+import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/models/TaskModel.dart';
+
+class TaskService {
+    Future<void> saveTasks(List<Taskmodel> tasks) async {
+      final prefs = await SharedPreferences.getInstance();
+      final save = tasks.map((tasks)=> jsonEncode(tasks.toJson())).toList();
+      await prefs.setStringList('key', save);
+    }
+
+    Future<List<Taskmodel>> loadTasks() async{
+      final prefs = await SharedPreferences.getInstance();
+      final string = prefs.getStringList("key") ?? [];
+      final load = string.map((string) => Taskmodel.fromJson(jsonDecode(string))).toList();
+      return load;
+    }
+
+    Future<void> addTasks(Taskmodel tasks) async {
+      final add = await loadTasks();
+      add.add(tasks);
+      final save = await saveTasks(add);
+    }
 }
