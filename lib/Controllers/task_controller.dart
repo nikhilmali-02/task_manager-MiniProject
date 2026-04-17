1651e7ec7ceca8a760206e989bf0fc67f1690a27
NikhilMali <nikhilpundalikmali@gmail.com>
2026-04-17 12:45:40 +0530

    17 April 2026 - task_controller.dart(toggleTask,deleteTask,updateTask),All controller methods are done.

--- lib/Controllers/task_controller.dart
+++ lib/Controllers/task_controller.dart
@@ -44,6 +44,52 @@ class TaskController extends ChangeNotifier {
     }
   }
 
-  Future<Taskmodel> toggleTask(List<Taskmodel> tasks) async {
+  Future<void> toggleTask(Taskmodel tasks) async {
+    try{
+      final toggled = tasks.copyWith(isCompleted: !tasks.isCompleted);
+      var result = await service.loadTasks();
+      result = result.map((item) => item == tasks ? toggled : item).toList();
+      await service.saveTasks(result);
+      await loadTask();
+      notifyListeners();
+    } catch (e){
+      error = "Falied to load";
+      notifyListeners();
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
 
+  Future<void> deleteTask(Taskmodel tasks) async {
+    try{
+      var result = await service.loadTasks();
+      result.removeWhere((item) => item == tasks);
+      await service.saveTasks(result);
+      await loadTask();
+      notifyListeners();
+    } catch (e){
+      error = "Falied to load";
+      notifyListeners();
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> updateTask(Taskmodel tasks) async {
+    try{
+      var result = await service.loadTasks();
+      result = result.map((item) => item == tasks ? tasks : item).toList();
+      await service.saveTasks(result);
+      await loadTask();
+      notifyListeners();
+    } catch (e){
+      error = "Falied to load";
+      notifyListeners();
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
 }
