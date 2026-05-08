6b3b47d9eaed7d1c60a2e6aa5273bfcd40336d67
NikhilMali <nikhilpundalikmali@gmail.com>
2026-05-08 10:01:55 +0530

    7 may 2026 - main, home screen and add addscreen

---
+++ lib/screens/AddTaskScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Addtaskscreen extends StatefulWidget{
+  @override
+  State<Addtaskscreen> createState() => AddTasks();
+
+}
+
+class AddTasks extends State<Addtaskscreen> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:task_manager/Controllers/task_controller.dart';
+import 'package:task_manager/main.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  State<HomeScreen> createState() => HomeScreen_State();
+}
+
+class HomeScreen_State extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      context.read<TaskController>().loadTask();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final controller = context.watch<TaskController>();
+
+    final allTask = [
+      ...controller.incompeletedList,
+      ...controller.completedList,
+    ];
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        context.go('/add');
+      }),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () => context.go('/setting'), icon: Icon(Icons.settings))
+        ],
+        title: Text('MyTasks'),
+      ),
+      body: controller.isLoading
+          ? Center(child: CircularProgressIndicator())
+          : controller.completedList.isEmpty && controller.incompeletedList.isEmpty
+              ? Center(child: Text('No Task Yet!'))
+              : ListView.builder(
+                  itemCount: allTask.length,
+                  itemBuilder: (context,index){
+                    final task = allTask[index];
+                    return Dismissible(key: Key(task.id),
+                        direction: DismissDirection.endToStart,
+                        onDismissed: (_) =>   context.read<TaskController>().deleteTask(task),
+                        child: Card(
+                          child: ListTile(
+                            title: Text(
+                                task.title,
+                              style: TextStyle(
+                                decoration: task.isCompleted ? TextDecoration.lineThrough : null
+                              ),
+                            ),
+                            subtitle: Text(task.subtitle ?? ''),
+                              leading: Checkbox(value: task.isCompleted,
+                                onChanged: (_) => context.read<TaskController>().toggleTask(task) ),
+                              trailing: Text(task.priority),
+                          ),
+                        )
+                    );
+                  },)
+
+    );
+  }
+}
