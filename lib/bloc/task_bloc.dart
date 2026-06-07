b5f00e37fe80fe1cb1cfcf5509d4e308e6d3ef89
NikhilMali <nikhilpundalikmali@gmail.com>
2026-06-07 12:09:56 +0530

    fix: add await to scheduleNotification, add notification to AddTaskEvent

--- lib/bloc/task_bloc.dart
+++ lib/bloc/task_bloc.dart
@@ -3,29 +3,28 @@ import 'package:task_manager/bloc/TaskState.dart';
 import 'package:task_manager/bloc/task_event.dart';
 import 'package:task_manager/services/firestore_service.dart';
 
-
 import '../services/notification_service.dart';
 
 class TaskBloc extends Bloc<TaskEvent, TaskState> {
-
   final _firestoreService = FirestoreService();
-  TaskBloc () : super(TaskLoadingState()) {
+  TaskBloc() : super(TaskLoadingState()) {
     on<LoadTasksEvent>((event, emit) async {
       emit(TaskLoadingState());
       try {
         final result = await _firestoreService.loadTasks();
         emit(TaskLoadedState(task: result));
-      } catch (e){
+      } catch (e) {
         emit(TaskErrorState(message: 'Failed to load'));
       }
     });
-    
+
     on<AddTaskEvent>((event, emit) async {
-      try{
+      try {
         await _firestoreService.addTask(event.task);
+        await NotificationService.scheduleNotification(event.task);
         final result = await _firestoreService.loadTasks();
         emit(TaskLoadedState(task: result));
-      } catch(e){
+      } catch (e) {
         emit(TaskErrorState(message: 'Failed to Add'));
       }
     });
@@ -35,28 +34,28 @@ class TaskBloc extends Bloc<TaskEvent, TaskState> {
         await _firestoreService.toggleTask(event.id);
         final result = await _firestoreService.loadTasks();
         emit(TaskLoadedState(task: result));
-      } catch (e){
+      } catch (e) {
         emit(TaskErrorState(message: 'Failed to Update'));
       }
     });
 
     on<DeleteTaskEvent>((event, emit) async {
-      try{
+      try {
         await _firestoreService.deleteTask(event.id);
         final result = await _firestoreService.loadTasks();
         emit(TaskLoadedState(task: result));
-      } catch(e){
+      } catch (e) {
         emit(TaskErrorState(message: 'Failed to Delete'));
       }
     });
 
     on<UpdateTaskEvent>((event, emit) async {
-      try{
+      try {
         await _firestoreService.updateTask(event.task);
+        await NotificationService.scheduleNotification(event.task);
         final result = await _firestoreService.loadTasks();
         emit(TaskLoadedState(task: result));
-        NotificationService.scheduleNotification(event.task);
-      } catch(e){
+      } catch (e) {
         emit(TaskErrorState(message: 'Failed to Update'));
       }
     });
